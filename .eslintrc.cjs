f59a730cd300098905c0a69ae0ec1e3c6f4d12b2
Prophet-88 <a8lewis@gmail.com>
2023-11-12 22:10:17 +0900

    First Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["prettier", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    quotes: "off",
+    "import/prefer-default-export": "off",
+    "no-use-before-define": "off",
+  },
+};
