b61cc24f4ca436c2cb9aa6b602560d161fb99e1c
Prophet-88 <a8lewis@gmail.com>
2023-11-23 17:21:29 +0900

    feat: header introduction & PC 3D modal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5 ">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I'm <span className="text-[#915eff]"> Lewi</span>
+          </h1>
+          <p className={`${styles.heroSubText}`}>
+            Frontend Engineer, <br />
+            Currently located in Tokyo, Japan.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
