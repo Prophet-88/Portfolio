06f76f145542686e772c7b415b6a3f61aae04761
[Prophet-88] <[a8lewis@gmail.com]>
2024-02-03 18:08:04 +0900

    feat: タイトル追加 + タイムラインの作成開始

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElements,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>My Experience</h2>
+      </motion.div>
+      <div className="mt-20 flex flex-col"></div>
+    </>
+  );
+};
 
-export default Experience
+export default SectionWrapper(Experience, "experience");
